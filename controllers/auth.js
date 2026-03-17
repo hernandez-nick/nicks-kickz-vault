d78fa41f578d6a6a310b7f3926054b8196494696
hernandeznick-ga <hernandeznick1990@gmail.com>
2026-03-17 03:12:23 -0700

    fixed induces routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,9 +15,7 @@ router.post("/sign-up", async (req, res) => {
     const foundUser = await User.findOne({ username });
     if (foundUser) {
       // If the username is already taken, we can send an error message back to the client.
-        throw new Error(
-            `Username already exists. Please choose a different ${username}.`,
-        );
+        throw new Error(`User with username ${username} already exists. Please choose a different username.`);
     }
     // The password and confirmPassword fields must match to verify there were no typos.
     if (password !== confirmPassword) {
@@ -27,14 +25,16 @@ router.post("/sign-up", async (req, res) => {
 
     const hashedPassword = bcrypt.hashSync(password, 8);
     const user = await User.create({
-        username,
-        hashedPassword,
+            username,
+            hashedPassword,
         });
         req.session.user = {
-        _id: user._id,
-        username: user.username,
+            _id: user._id,
+            username: user.username,
         };
-        res.redirect("/shoes");
+        req.session.save(() => {
+            res.redirect("/shoes");
+        });
     } catch (error) {
         res.render("auth/sign-up.ejs", { message: error.message });
     }
@@ -49,22 +49,24 @@ router.post("/sign-in", async (req, res) => {
     try {
         const foundUser = await User.findOne({ username });
         if (!foundUser) {
-        throw new Error(
-            `User with the username ${username} does not exist. Please sign up first.`,
+            throw new Error(
+                `User with the username ${username} does not exist. Please sign up first.`,
         );
         }
         const isValidPassword = bcrypt.compareSync(
-        password,
-        foundUser.hashedPassword,
+            password,
+            foundUser.hashedPassword,
         );
         if (!isValidPassword) {
-        throw new Error("Invalid username or password. Please try again.");
+            throw new Error("Invalid username or password. Please try again.");
         }
         req.session.user = {
-        _id: foundUser._id,
-        username: foundUser.username,
+            _id: foundUser._id,
+            username: foundUser.username,
         };
-        res.redirect("/shoes");
+        req.session.save(() => {
+            res.redirect("/shoes");
+        });
     } catch (error) {
         res.render("auth/sign-in.ejs", { message: error.message });
     }
--- controllers/shoes.js
+++ controllers/shoes.js
@@ -3,150 +3,158 @@ const Shoe = require("../models/shoe.js");
 
 const router = express.Router();
 
-// Index Route - GET /shoes
 module.exports = router;
 
 // Index - GET /shoes - get all the shoes and send back a page
 router.get("/", async (req, res) => {
-    try {
+  try {
     // Find all the shoes in the database and send them to the index.ejs page as a variable called shoes
     const shoes = await Shoe.find({
-        owner: req.session.user._id,
+      owner: req.session.user._id,
     });
-        res.render("shoes/index.ejs", { shoes: shoes });
-    } catch (error) {
-        res.render("error.ejs", { message: error.message });
-    }
+    res.render("shoes/index.ejs", { shoes: shoes });
+  } catch (error) {
+    res.render("error.ejs", { message: error.message });
+  }
 });
 
 // New - GET /shoes/new - send a form page to create a new shoe
-router.get("/new", (req, res) => {
-res.render("shoes/new.ejs");
+router.get("/new", async (req, res) => {
+  res.render("shoes/new.ejs");
 });
 
 // Delete - Delete /shoes/:shoeId - delete some shoes based on the param passed
 router.delete("/:shoeId", async (req, res) => {
-    try {
+  try {
     // Find the shoe in the database with the id from the url params and delete it, then redirect back to the index page
     await Shoe.findOneAndDelete({
-        _id: req.params.shoeId,
-        owner: req.session.user._id,
+      _id: req.params.shoeId,
+      owner: req.session.user._id,
     });
-        res.redirect("/shoes");
-    } catch (error) {
-        res.render("error.ejs", { message: error.message });
-    }
+    res.redirect("/shoes");
+  } catch (error) {
+    res.render("error.ejs", { message: error.message });
+  }
+});
+
+// GET /shoes/:shoeId/confirm_delete - show confirmation before deleting
+router.get("/:shoeId/confirm_delete", async (req, res) => {
+  try {
+    const foundShoe = await Shoe.findOne({
+      _id: req.params.shoeId,
+      owner: req.session.user._id,
+    });
+    if (!foundShoe)
+      throw new Error(
+        "Failed to find that shoe, please click back and try again",
+      );
+
+    res.render("shoes/shoe_confirm_delete.ejs", {
+      shoe: foundShoe,
+    });
+  } catch (error) {
+    res.render("error.ejs", { message: error.message });
+  }
 });
 
 // Update - PUT /shoes/:shoeId - update some shoes based on the param passwed and req.body
 router.put("/:shoeId", async (req, res) => {
-    try {
+  try {
     // Find the shoe in the database with the id from the url params and update it with the data from req.body, then redirect back to the index page
-        await Shoe.findOneAndUpdate(
-            { _id: req.params.shoeId, owner: req.session.user._id },
-            req.body,
-            { new: true },
-        );
-        res.redirect(`/shoes/${req.params.shoeId}`);
-    } catch (error) {
-        res.render("error.ejs", { message: error.message });
-    }
+    const shoeData = {
+      ...req.body,
+      isDeadstock: req.body.isDeadstock === "on",
+    };
+    await Shoe.findOneAndUpdate(
+      { _id: req.params.shoeId, owner: req.session.user._id },
+      shoeData,
+      { new: true },
+    );
+    res.redirect(`/shoes/${req.params.shoeId}`);
+  } catch (error) {
+    res.render("error.ejs", { message: error.message });
+  }
 });
 
 // Create - POST /shoes - take data from shoes/new form and add a new shoe to the database
 router.post("/", async (req, res) => {
-    try {
-    // Check for an empty name field, if it's empty throw a manual error to be caught by the catch block
-    const { name, brand, price, color, description } = req.body;
-    // If I trim the name and color and they are falsy (empty string, null, undefined) then throw an error
-    if (!name || !brand || !color) {
-        return res.render("shoes/new.ejs", {
-            message:
-                "Name, brand, and color fields cannot be empty, please try again",
-        });
-    }
-
-    if (description && description.length > 100) {  
-        return res.render("shoes/new.ejs", {
-        message:
-            "Description cannot be longer than 100 characters, please try again",
-    });
-}
-
-// Give the form data to the model.create to make a new mongodb document
-    await Shoe.create({
-        ...req.body,
-        owner: req.session.user._id,
-    });
+  try {
+    const shoeData = {
+      ...req.body,
+      owner: req.session.user._id,
+      isDeadstock: req.body.isDeadstock === "on",
+    };
+    // Give the form data to the model.create to make a new mongodb document
+    await Shoe.create(shoeData);
     // Redirect the user back to the index page after creating the new shoe
-        res.redirect("/shoes");
-    } catch (error) {
-        res.render("error.ejs", { message: error.message });
-    }
+    res.redirect("/shoes");
+  } catch (error) {
+    res.render("error.ejs", { message: error.message });
+  }
 });
 
-// GET /shoes/:shoeId/confirm_delete - show a confirmation page before deleting a shoe
-router.get("/:shoeId/confirm_delete", async (req, res) => {
-    try {
+// GET /shoes/:shoeId/ - show one specific shoe
+router.get("/:shoeId/", async (req, res) => {
+  try {
     // Find the shoe in the database with the id from the url params and send it to the show.ejs page as a variable called shoe
     const foundShoe = await Shoe.findOne({
-        _id: req.params.shoeId,
-        owner: req.session.user._id,
+      _id: req.params.shoeId,
+      owner: req.session.user._id,
     });
     // If no shoe is found, throw a manual error to be caught by the catch block
     if (!foundShoe)
-        throw new Error(
+      throw new Error(
         "Failed to find that shoe, please click back and try again",
-        );
+      );
 
-        res.render("shoes/shoe_confirm_delete.ejs", {
-        shoe: foundShoe,
+    res.render("shoes/show.ejs", {
+      shoe: foundShoe,
     });
-    } catch (error) {
-        res.render("error.ejs", { message: error.message });
-    }
+  } catch (error) {
+    res.render("error.ejs", { message: error.message });
+  }
 });
 
 // Edit - GET /shoes/:shoeId/edit - edit a specific shoe
 router.get("/:shoeId/edit", async (req, res) => {
-    try {
+  try {
     // Find the shoe in the database with the id from the url params and send it to the show.ejs page as a variable called shoe
     const foundShoe = await Shoe.findOne({
-        _id: req.params.shoeId,
-        owner: req.session.user._id,
+      _id: req.params.shoeId,
+      owner: req.session.user._id,
     });
     // If no shoe is found, throw a manual error to be caught by the catch block
     if (!foundShoe)
-        throw new Error(
+      throw new Error(
         "Failed to find that shoe, please click back and try again",
-        );
+      );
 
     res.render("shoes/edit.ejs", {
-        shoe: foundShoe,
+      shoe: foundShoe,
     });
-    } catch (error) {
+  } catch (error) {
     res.render("error.ejs", { message: error.message });
-    }
-    });
+  }
+});
 
 // Show - GET /shoes/:shoeId - show one specific shoe
 router.get("/:shoeId", async (req, res) => {
-    try {
+  try {
     // Find the shoe in the database with the id from the url params and send it to the show.ejs page as a variable called shoe
     const foundShoe = await Shoe.findOne({
-        _id: req.params.shoeId,
-        owner: req.session.user._id,
+      _id: req.params.shoeId,
+      owner: req.session.user._id,
     });
     // If no shoe is found, throw a manual error to be caught by the catch block
     if (!foundShoe)
-        throw new Error(
+      throw new Error(
         "Failed to find that shoe, please click back and try again",
-        );
+      );
 
-res.render("shoes/show.ejs", {
-    shoe: foundShoe,
-});
-} catch (error) {
-res.render("error.ejs", { message: error.message });
-}
+    res.render("shoes/show.ejs", {
+      shoe: foundShoe,
+    });
+  } catch (error) {
+    res.render("error.ejs", { message: error.message });
+  }
 });
